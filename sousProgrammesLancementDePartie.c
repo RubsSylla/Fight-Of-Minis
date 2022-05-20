f08c836a2b7e733688d029813cf4a97803d2829f
iriscrd <96662145+iriscrd@users.noreply.github.com>
2022-05-20 17:39:21 +0200

    Update sousProgrammesLancementDePartie.c

--- sousProgrammesLancementDePartie.c
+++ sousProgrammesLancementDePartie.c
@@ -4,10 +4,18 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
 {
     int a=0;
     int N;
+    int nombreJoueurs=0;
     int choix=0;
+    int choixClasse=0;
     int saisie=0;
+    int choixDebutJeu=0;
 
-    char *nomsJoueurs;
+    BITMAP* feuille;
+    feuille = load_bitmap("Feuille.bmp", NULL);
+
+    int statut;
+
+    t_joueurs *JOUEURS;
 
     clear(buffer);
     blit(lancement,buffer,0,0,0,0,800,600);
@@ -26,8 +34,8 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
             blit(buffer,screen,0,0,0,0,800,600);
             if(mouse_b &1)
             {
-                N=2;
-                printf("le nombre de joueurs pour cette partie est de : %d ", N);
+                nombreJoueurs=2;
+                printf("le nombre de joueurs pour cette partie est de : %d ", nombreJoueurs);
                 choix=1;
                 rest(30);
             }
@@ -41,8 +49,8 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
                 blit(buffer,screen,0,0,0,0,800,600);
                 if(mouse_b &1)
                 {
-                    N=3;
-                    printf("le nombre de joueurs pour cette partie est de : %d ", N);
+                    nombreJoueurs=3;
+                    printf("le nombre de joueurs pour cette partie est de : %d ", nombreJoueurs);
                     choix=1;
                     rest(30);
                 }
@@ -56,8 +64,8 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
                     blit(buffer,screen,0,0,0,0,800,600);
                     if(mouse_b &1)
                     {
-                        N=4;
-                        printf("le nombre de joueurs pour cette partie est de : %d ", N);
+                        nombreJoueurs=4;
+                        printf("le nombre de joueurs pour cette partie est de : %d ",nombreJoueurs);
                         choix=1;
                         rest(30);
                     }
@@ -75,7 +83,9 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
                 }
             }
         }
+
         a=test1(lancement, regles, options, conception, buffer, image, fond,pageJeu);
+
         if(a==1)
         {
             clear(buffer);
@@ -84,21 +94,24 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
 
         if(choix==1)
         {
-            nomsJoueurs=(char*)malloc(N*sizeof(char*));
+            JOUEURS=(t_joueurs*)malloc(nombreJoueurs*sizeof(t_joueurs));
+
             textprintf_ex(buffer, font, 415, 140, marron,-1, "%s", "Veuillez suivre les instructions sur ");
             textprintf_ex(buffer, font,415,150,marron,-1,"%s","la fenetre de commande : ");
             blit(buffer,screen,0,0,0,0,800,600);
             printf("\n");
-            for(int i=0 ; i<N ; i++)
+
+
+            for(int i=0 ; i<nombreJoueurs ; i++)
             {
                 printf("saisir le nom du %d e joueur : ", i+1);
                 fflush(stdin);
-                scanf("%s", &nomsJoueurs[i]);
+                scanf("%s", &JOUEURS[i].nom);
             }
             printf("\nLES NOMS DES JOUEURS SONT :\n");
             for(int j=0 ; j<N ; j++)
             {
-                printf("- %c\n", nomsJoueurs[j]);
+              printf("- %s\n", JOUEURS[j].nom);
 
             }
             printf("\n----------NOMS DES JOUEURS ENREGISTRES----------\n");
@@ -108,31 +121,125 @@ void lancementPartie(BITMAP* lancement, BITMAP* regles, BITMAP* options, BITMAP*
 
         if(saisie==1)
         {
-            rectfill(buffer, 415, 200, 700, 230, blanc);
-            textprintf_ex(buffer, font, 442, 215, marron,-1, "%s", "APPUYER POUR DEBUTER LA PARTIE");
-            blit(buffer,screen,0,0,0,0,800,600);
+            for(int i=0 ; i<nombreJoueurs ; i++)
+            {
+                mouse_x=10;
+                mouse_y=10;
 
+                choixClasse=0;
+
+                while(choixClasse==0)
+                {
+                    if(statut == 0)
+                    {
+                        textprintf_ex(buffer,font,442,215,marron,-1,"Le joueur %d choisi sa classe :\n", i+1);
+
+                        rectfill(buffer,405,250,475,265,blanc);
+                        textprintf_ex(buffer,font,407,255,marron,-1,"%s","GUERRIER");
+                        rectfill(buffer,479,250,547,265,blanc);
+                        textprintf_ex(buffer,font,481,255,marron,-1,"%s","SORCIERE");
+                        rectfill(buffer,551,250,650,265,blanc);
+                        textprintf_ex(buffer,font,553,255,marron,-1,"%s","GUERRISSEUSE");
+                        rectfill(buffer,654,250,720,265,blanc);
+                        textprintf_ex(buffer,font,656,255,marron,-1,"%s","CHASSEUR");
+
+                        blit(buffer,screen,0,0,0,0,800,600);
+                    }
+
+                    if(((mouse_x>=405)&&(mouse_x<=475))&&((mouse_y>=250)&&(mouse_y<=265)))
+                    {
+                        statut = 1;
+                        rectfill(buffer,405,250,475,265,marron);
+                        textprintf_ex(buffer,font,407,255,blanc,-1,"%s","GUERRIER");
+                        blit(buffer,screen,0,0,0,0,800,600);
+                        if(mouse_b &1)
+                        {
+                            JOUEURS[i].classes.guerrier=1;
+                            choixClasse=1;
+                            printf("le joueur %d a choisi la classe : guerrier\n", i+1);
+                            break;
+                        }
+                    }
+                    else if(((mouse_x>=479)&&(mouse_x<=547))&&((mouse_y>=250)&&(mouse_y<=265)))
+                    {
+                        statut = 1;
+                        rectfill(buffer,479,250,547,265,marron);
+                        textprintf_ex(buffer,font,481,255,blanc,-1,"%s","SORCIERE");
+                        blit(buffer,screen,0,0,0,0,800,600);
+                        if(mouse_b &1)
+                        {
+                            JOUEURS[i].classes.sorciere=1;
+                            choixClasse=1;
+                            printf("le joueur %d a choisi la classe : sorciere\n", i+1);
+                            break;
+                        }
+                    }
+                    else if(((mouse_x>=551)&&(mouse_x<=650))&&((mouse_y>=250)&&(mouse_y<=265)))
+                    {
+                        statut = 1;
+                        rectfill(buffer,551,250,650,265,marron);
+                        textprintf_ex(buffer,font,553,255,blanc,-1,"%s","GUERRISSEUSE");
+                        blit(buffer,screen,0,0,0,0,800,600);
+                        if(mouse_b &1)
+                        {
+                            JOUEURS[i].classes.guerrisseuse=1;
+                            choixClasse=1;
+                            printf("le joueur %d a choisi la classe : guerrisseuse\n\n", i+1);
+                            break;
+                        }
+                    }
+                    else if(((mouse_x>=654)&&(mouse_x<=720))&&((mouse_y>=250)&&(mouse_y<=265)))
+                    {
+                        statut = 1;
+                        rectfill(buffer,654,250,720,265,marron);
+                        textprintf_ex(buffer,font,656,255,blanc,-1,"%s","CHASSEUR");
+                        blit(buffer,screen,0,0,0,0,800,600);
+                        if(mouse_b &1)
+                        {
+                            JOUEURS[i].classes.chasseur=1;
+                            choixClasse=1;
+                            printf("le joueur %d a choisi la classe : chasseur", i+1);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        statut = 0;
+                    }
+
+
+
+                }
+            }
+
+            rectfill(buffer, 415, 400, 700, 430, blanc);
+            textprintf_ex(buffer, font, 442, 415, marron,-1, "%s", "APPUYER POUR DEBUTER LA PARTIE");
+            blit(buffer,screen,0,0,0,0,800,600);
             a=test1(lancement, regles, options, conception, buffer, image, fond,pageJeu);
+            printf("%d", a);
+
             while(a!=1)
             {
-                if(((mouse_x>=415)&&(mouse_x<=700))&&((mouse_y>=200)&&(mouse_y<=230)))
+                if(((mouse_x>=415)&&(mouse_x<=700))&&((mouse_y>=400)&&(mouse_y<=430)))
                 {
-                    rectfill(buffer, 415, 200, 700, 230, marron);
-                    textprintf_ex(buffer, font, 442, 215, blanc,-1, "%s", "APPUYER POUR DEBUTER LA PARTIE");
+                    rectfill(buffer, 415, 400, 700, 430, marron);
+                    textprintf_ex(buffer, font, 442, 415, blanc,-1, "%s", "APPUYER POUR DEBUTER LA PARTIE");
                     blit(buffer,screen,0,0,0,0,800,600);
-                    if(mouse_b &1)
+
+                    if(mouse_b & 1)
                     {
                         blit(pageJeu,screen,0,0,0,0,800,600);
                         rest(1000);
                         a=1;
-                        menu(image,fond,buffer,lancement,regles,options,conception,pageJeu);   ///retour au menu principal mais il faudra mettre la fonction du jeu
+
+                        ///Jeu(JOUEURS,buffer,feuille,nombreJoueurs);
+                        //menu(image,fond,buffer,lancement,regles,options,conception,pageJeu);   ///retour au menu principal mais il faudra mettre la fonction du jeu
                     }
                 }
             }
         }
     }
 }
-
 
 
 
